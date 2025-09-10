c78f1967a024decae1ec871e653a2f0de8564160
Densky <densky.bojkul@gmail.com>
2025-09-11 02:15:05 +0700

    Update SplashActivity.java

--- app/src/main/java/com/nunucore/corearsitek/SplashActivity.java
+++ app/src/main/java/com/nunucore/corearsitek/SplashActivity.java
@@ -3,17 +3,22 @@ package com.nunucore.corearsitek;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 2000; // 2 detik
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(() -> {
-            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
             finish();
-        }, 2000);
+        }, SPLASH_DELAY);
     }
 }
